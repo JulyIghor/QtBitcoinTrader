39ecb194685d2c9fa5e2eefc1221db8eaddc79cf
IGHOR <julyighor@gmail.com>
2013-08-19 03:06:30 +0300

    v1.07.65 Beta

--- src/main.cpp
+++ src/main.cpp
@@ -98,7 +98,7 @@ int main(int argc, char *argv[])
 	julyTranslator=new JulyTranslator;
 	appDataDir_=new QByteArray();
 	appVerIsBeta_=new bool(false);
-	appVerStr_=new QByteArray("1.0764");
+	appVerStr_=new QByteArray("1.0765");
 	appVerReal_=new double(appVerStr.toDouble());
 	if(appVerStr.size()>4)
 	{ 
