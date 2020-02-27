8abef884153052218570f1f8398c026cf6999dd2
Ighor July <julyighor@gmail.com>
2020-02-27 18:10:08 +0100

    2020

--- src/apptheme.h
+++ src/apptheme.h
@@ -1,6 +1,6 @@
 //  This file is part of Qt Bitcoin Trader
 //      https://github.com/JulyIGHOR/QtBitcoinTrader
-//  Copyright (C) 2013-2019 July Ighor <julyighor@gmail.com>
+//  Copyright (C) 2013-2020 July Ighor <julyighor@gmail.com>
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
